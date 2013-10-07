0081dc16277e1439d48d56e76442e52c39ee87a9
Simon Altschuler <simon@altschuler.dk>
2013-10-07 09:00:48 +0200

    simplified shell script, added rm

--- engine.rb
+++ engine.rb
@@ -1,3 +1,36 @@
+# yay hooray lulz from http://stackoverflow.com/questions/1489183/colorized-ruby-output
+class String
+  def colorize(color_code)
+    "\e[#{color_code}m#{self}\e[0m"
+  end
+
+  def red
+    colorize(31)
+  end
+
+  def green
+    colorize(32)
+  end
+
+  def yellow
+    colorize(33)
+  end
+end
+
+def wrn!(msg)
+  puts 'warning: '.yellow + msg
+  exit 1
+end
+
+def err!(msg)
+  puts 'err: '.red + msg
+  exit 1
+end
+
+def yay(msg)
+  puts 'yay! '.green + msg
+end
+
 # define 
 @alias_file = "#{ENV['HOME']}/.warps"
 
@@ -7,31 +40,54 @@ def add_target(name, target)
   end
 end
 
-def rem_target
-  
+def rem_target(dest)
+  all = ''
+  found = false
+  File.readlines(@alias_file).each do |line|
+    if not line.strip().empty? then
+      ali = line.split(':')
+      name = ali[0]
+      dir = ali[1]
+      if name == dest then
+        found = true
+      else
+        all += line
+      end
+    end
+  end
+
+  if not found then
+    wrn! "warp does not exist '#{dest}'"
+  else
+    File.open(@alias_file, 'w') { |file| file.write(all) }
+    yay "removed warp '#{dest}'"
+  end
 end
 
 def warp(dest)
   found = false
   File.readlines(@alias_file).each do |line|
-    ali = line.split(':')
-    name = ali[0].strip!
-    dir = ali[1].strip!
-    if ali[0] == dest then
-      puts dir
-      found = true
+    if not line.strip().empty? then
+      ali = line.split(':')
+      name = ali[0].strip!
+      dir = ali[1].strip!
+      if name == dest then
+        puts dir
+        found = true
+      end
     end
   end
 
   if not found then
-    puts "no warps named '#{dest}'"
-    exit 1
+    err! "warp does not exist '#{dest}'"
   end
 end
 
 def list_targets
   File.readlines(@alias_file).each do |line|
-    puts line.sub!(':', ' -> ').strip! + '\n\r'
+    if not line.strip().empty? then
+      puts "%-6s %s" % line.split(':')
+    end
   end
 end
 
@@ -39,15 +95,19 @@ end
 case ARGV[0]
 when 'add'
   if ARGV.length < 2 then 
-    exit 1
+    err! 'no name given'
   else
     dir = Dir.pwd
     add_target ARGV[1], dir
-    puts "added '#{ARGV[1]}' pointing to #{dir}"
+    yay "added '#{ARGV[1]}' warping to #{dir}"
   end
 
 when 'rm'
-  rem_target
+  if ARGV.length < 2 then 
+    err! 'don\'t know what to remove'
+  else
+    rem_target ARGV[1]
+  end
 
 when 'ls'
   list_targets
